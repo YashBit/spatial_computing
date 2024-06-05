6f70c70eb074f6e82a32b0a4a6de519169d097b7
Yash Bharti <yashbharti@Yashs-MacBook-Pro.local>
2024-06-05 20:02:34 +0530

    Price Change Problem Solved

--- spatial_app/src/components/my_form.tsx
+++ spatial_app/src/components/my_form.tsx
@@ -18,26 +18,13 @@ import {
 import { Card, CardContent, CardHeader, CardTitle } from "../components/ui/card";
 import { uploadFileToS3 } from "../server/services/s3Upload";
 
-
-function videoPriceCalculator(videoLengthInSeconds: number): number {
-  const basePrice = 4.75;
-  const additionalMinutePrice = 1;
-  const videoLengthInMinutes = videoLengthInSeconds / 60;
-  const additionalMinutes = Math.max(0, Math.ceil(videoLengthInMinutes - 3));
-  const additionalPrice = additionalMinutes * additionalMinutePrice;
-  const totalPrice = basePrice + additionalPrice;
-  console.log("VIDEO LENGTH IS:")
-  console.log(videoLengthInSeconds);
-  console.log("TOTAL PRICE CALCULATED IS");
-  console.log(totalPrice)
-  return totalPrice;
-}
-
 export function ProfileForm() {
   const MAX_IMAGE_SIZE = 2147483648; // 2 GB in bytes
   const ALLOWED_VIDEO_TYPES = [
+    "video/mov",
     "video/mp4",
     "video/avi",
+    "video/*",
   ];
 
   const formSchema = z.object({
@@ -66,60 +53,58 @@ export function ProfileForm() {
     resolver: zodResolver(formSchema),
   });
   const router = useRouter();
-  const [totalDuration, setTotalDuration] = useState(0); // State for storing the total duration of the uploaded videos
-  const [calculatedPrice, setCalculatedPrice] = useState(0); // State for storing the calculated price
-  const [selectedVideos, setSelectedVideos] = useState<FileList | null>(null);
-  
-  const handleVideoFileChange = (videoFile) => {
-      Array.from(videoFile).forEach((video) => {
-        const videoElement = document.createElement('video');
-        videoElement.src = URL.createObjectURL(video);
-        const durationInSeconds = videoElement.duration;
-        console.log("DURATION");
-        console.log(durationInSeconds);
-      });
-    // const videoElement = document.createElement('video');
-    // videoElement.src = URL.createObjectURL(videoFile);
-  
-    // videoElement.onloadedmetadata = () => {
-    //   // Duration is available after metadata has been loaded
-    //   const durationInSeconds = videoElement.duration;
-    //   console.log("Duration in seconds:", durationInSeconds);
-    //   const totalPrice = videoPriceCalculator(durationInSeconds);
-    //   console.log("Total price:", totalPrice);
-    //   setCalculatedPrice(totalPrice);
-    // };
-  };
-  
+  // const [totalDuration, setTotalDuration] = useState(0); // State for storing the total duration of the uploaded videos
+  const [calculatedPrice, setCalculatedPrice] = useState(0);
+
+  function videoPriceCalculator(videoLengthInSeconds: number): number {
+    const basePrice = 4.75;
+    const additionalMinutePrice = 1;
+    const videoLengthInMinutes = videoLengthInSeconds / 60;
+    const additionalMinutes = Math.max(0, Math.ceil(videoLengthInMinutes - 3));
+    const additionalPrice = additionalMinutes * additionalMinutePrice;
+    const totalPrice = basePrice + additionalPrice;
+    console.log("VIDEO LENGTH IS:")
+    console.log(videoLengthInSeconds);
+    console.log("TOTAL PRICE CALCULATED IS");
+    console.log(totalPrice)
+    return totalPrice;
+  }
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    try {
+    try{
       const videos = values["videos"];
+      const video = values["videos"][0];
       const email = values["email"];
       const name = values["name"];
       const bucketName = "spatialapp";
       const folderName = "user_data/";
-  
-      // let totalDurationInSeconds = 0;
-      // Array.from(videos).forEach((videoFile) => {
-      //   const videoElement = document.createElement('video');
-      //   videoElement.src = URL.createObjectURL(videoFile);
+      
+    Array.from(videos).forEach((videoFile) => {
+      const videoElement = document.createElement('video');
+      videoElement.src = URL.createObjectURL(videoFile);
+      
+      // Listen for the metadata loaded event
+      // videoElement.onloadedmetadata = () => {
       //   const durationInSeconds = videoElement.duration;
-      //   totalDurationInSeconds += durationInSeconds;
-      // });
-  
-      // const totalPrice = videoPriceCalculator(totalDurationInSeconds);
-      // setTotalDuration(totalDurationInSeconds);
-      // setCalculatedPrice(totalPrice);
-  
-      // Other logic for submission...
+      //   console.log("Duration in seconds:", durationInSeconds);
+      //   const totalPrice = videoPriceCalculator(durationInSeconds);
+      //   setCalculatedPrice(totalPrice);
+      // };
+
+
+    });
+      
+
+
+      // const fileUrl = await uploadFileToS3(videos, email, name, bucketName, folderName);
+
+      // console.log("Uploaded file URL:", fileUrl);
+
+      // router.push("convert_now/success");
     } catch (error) {
       console.error("Error submitting form:", error);
     }
   };
-  
 
-      // const fileUrl = await uploadFileToS3(videos, email, name, bucketName, folderName);
-      // console.log("Uploaded file URL:", fileUrl);
   return (
     <section className="flex flex-col gap-8 xl:gap-10 text-xlg">
       <Card className="text-black bg-gray-80">
@@ -128,7 +113,7 @@ export function ProfileForm() {
         </CardHeader>
         <CardContent className="text-center off-white-text text-lg">
           <p>Base Price: $4.75 for 3 minutes</p>
-          <p>+$1.00 / minute after base price</p>
+          <p>+$1 / minute after base price</p>
           {/* <p>Total Duration: {totalDuration} seconds</p> Display the total duration of the uploaded videos */}
         </CardContent>
       </Card>
@@ -174,30 +159,52 @@ export function ProfileForm() {
 
               return (
                 <FormItem>
-                  <FormLabel className="text-core_heading">Videos - mp4, avi </FormLabel>
+                  <FormLabel className="text-core_heading">Videos</FormLabel>
                   <FormControl>
-                  <Input
+                    <Input
                       type="file"
                       accept="video/*"
                       multiple={true}
                       disabled={form.formState.isSubmitting}
                       {...field}
                       onChange={(event) => {
-                        const selectedFiles = event.target.files;
-                        if (selectedFiles) {
-                          handleVideoFileChange(selectedFiles);
+                        const dataTransfer = new DataTransfer();
+
+                        if (videos) {
+                          Array.from(videos).forEach((video) =>
+                            dataTransfer.items.add(video)
+                          );
                         }
-                        onChange(selectedFiles); // Update form state with selected files
+
+                        Array.from(event.target.files!).forEach((video) =>
+                          dataTransfer.items.add(video)
+                        );
+                        Array.from(event.target.files!).forEach((videoFile) => {
+                          const videoElement = document.createElement('video');
+                          videoElement.src = URL.createObjectURL(videoFile);
+                          
+                          // Listen for the metadata loaded event
+                          videoElement.onloadedmetadata = () => {
+                            const durationInSeconds = videoElement.duration;
+                            console.log("Duration in seconds:", durationInSeconds);
+                            const totalPrice = videoPriceCalculator(durationInSeconds);
+                            setCalculatedPrice(totalPrice);
+                          };
+                    
+                    
+                        });
+                        const newFiles = dataTransfer.files;
+                        onChange(newFiles);
                       }}
                     />
-                </FormControl>
-
+                  </FormControl>
                   <FormMessage />
                 </FormItem>
               );
             }}
           />
-          <div className="flex flex-col gap-5 sm:flex-row">
+
+            <div className="flex flex-col gap-5 sm:flex-row">
               <div className="flex flex-col gap-2 w-full">
                 <p className="text-left text-core_heading font-semibold">Total Price: ${calculatedPrice.toFixed(2)}</p>
               <Button
@@ -220,4 +227,3 @@ export function ProfileForm() {
     </section>
   );
 }
-
